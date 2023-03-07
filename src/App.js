4b578ec4d2787ab9c0d7e78a1c9a782dcbce919e
EricHOfla <ericofla1@gmail.com>
2023-03-08 00:39:21 +0200

    webapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import JsonData from "./data.json";
+class Countries extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      getdata:''
+    };
+  }
+   componentDidMount() {
+    this.setState({data:JsonData})
+  }
+  searchdata=(event)=>{
+    const getdata=event.target.value.toLowerCase()
+    this.setState({getdata:getdata})
+  
+  }
+  
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+  const filtereddata=JsonData.filter((jsonItem)=>(
+   jsonItem.name.toLocaleLowerCase().includes(this.state.getdata)
+  ))
+
+    return( <div className='grid grid-cols-4   justify-end p-9 w-full mt-8'>
+      {console.log(this.state.getdata)}
+      <div>
+     <input type='text' placeholder="search....." className='h-6' onChange={this.searchdata} />
+     
+     </div>
+      {filtereddata.map((dataitem)=>(
+      
+        <div className='bg-slate-600'>
+       <img src={dataitem.flag} className='h-20 p-3 '></img>
+          <p>{dataitem.name}</p>
+          <p>population:{dataitem.population}</p>
+          <p>region:{dataitem.region}</p>
+          <p>capital:{dataitem.capital}</p>
+          
+        </div>
+      ))}
+    </div>)
+  }
 }
-
-export default App;
+export default Countries;
